e2860d1032106972705697afc4e98a704a719982
HailMuffins <rafaelsm123@outlook.com>
2022-04-13 14:45:27 -0300

    Finish

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    let sum = 0;
 
+    if(min < 0 || max < 0) {
+        return 'ERROR';
+    } else if(isNaN(min) || isNaN(max)) {
+        return 'ERROR';
+    } else if(typeof min === 'string' || typeof max === 'string') {
+        return 'ERROR';
+    } else if (min > max) {
+        for(let i = min; i >= max; i--) {
+            sum += i;
+        }
+    } else {
+        for(let i = min; i <= max; i++) {
+            sum += i;
+        }
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
